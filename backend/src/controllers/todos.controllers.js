0008bc8dd01c1620e35104bfc9aecbde08adf8d9
Quiroga J <juanqi3489@gmail.com>
2024-09-18 12:44:19 -0300

    Se realizaron modificaciones para corregir errores

--- backend/src/controllers/todos.controllers.js
+++ backend/src/controllers/todos.controllers.js
@@ -1,6 +1,5 @@
 import { database } from "../db/database.js";
 
-// Obtener todas las tareas
 export const getAllTodosCtrl = (req, res) => {
   if (!req.user) {
     return res.status(401).json({ message: "No autenticado" });
@@ -15,54 +14,79 @@ export const deleteTaskCtrl = (req, res) => {
   const id = req.user.id
   const deleteTd = database.todos.findIndex((todo) => todo.id === id)
   if (deleteTd === -1) {
-    res.json({ mensaje: "Ese id de todos no existe" })
+    res.json({ mensaje: "Tarea no encontrada"})
   } else {
     database.todos.splice(deleteTd, 1);
-    res.json({ mensaje: "Se eliminó el libro elegido" })
+    res.json({ mensaje: "Se eliminó la tarea elegida" })
   }
 }
-
-// export const tareasXid = (req, res) => {
-//   const id = parseInt(req.params.id);
-//   const usuarioId = database.todos.find((user) => user.id === id)
-//   if (!usuarioId) {
-//     return res.status(404).json({ message: "Tarea no encontrada" });
-//   } {
-//     res.json(usuarioId)
-//   }
-// }
 
 export const updateTaskCtrl = (req, res) => {
   const { title, completed } = req.body;
   const isCompleteValue = completed === true || completed === 'true' ? 1 : 0;
 
-  // Buscar el índice de la tarea que se desea editar
+  if (!title) {
+    return res.status(400).json({ message: "El título es requerido" });
+  } else if (typeof title !== "string") {
+    return res.status(400).json({ message: "El título debe ser un string" });
+  } else if (title.length < 3) {
+    return res
+      .status(400)
+      .json({ message: "El título debe tener al menos 3 caracteres" });
+  }
+
+  if (completed === undefined) {
+    return res
+      .status(400)
+      .json({ message: "El estado de la tarea es requerido" });
+  } else if (typeof completed !== "boolean") {
+    return res
+      .status(400)
+      .json({ message: "El estado de la tarea debe ser un booleano" });
+  }
+
   const todoIndex = database.todos.findIndex((todo) => todo.id === parseInt(req.params.id, 10));
 
   if (todoIndex === -1) {
-    // Si no se encuentra la tarea, devolver un error 404
     return res.status(404).json({ msg: "Tarea no encontrada" });
   } else {
-    // Actualizar la tarea en el índice encontrado
     database.todos[todoIndex].title = title;
     database.todos[todoIndex].completed = isCompleteValue;
 
-    // Devolver una respuesta exitosa
     return res.json({ msg: "Tarea actualizada correctamente", todo: database.todos[todoIndex] });
   }
-
 }
 
 export const createTaskCtrl = (req, res) => {
   const { title, completed } = req.body;
+
+  if (!title) {
+    return res.status(400).json({ message: "El título es requerido" });
+  } else if (typeof title !== "string") {
+    return res.status(400).json({ message: "El título debe ser un string" });
+  } else if (title.length < 3) {
+    return res
+      .status(400)
+      .json({ message: "El título debe tener al menos 3 caracteres" });
+  }
+
+  if (completed === undefined) {
+    return res
+      .status(400)
+      .json({ message: "El estado de la tarea es requerido" });
+  } else if (typeof completed !== "boolean") {
+    return res.status(400).json({ message: "El estado de la tarea debe ser un booleano" });
+  }
+
+
   const newTodo = {
-    id: database.todos.length + 1, // Asumiendo que el ID es secuencial
+    id: database.todos.length + 1,
     title,
     completed,
-    owner: req.user.id // Asumiendo que tienes la ID del usuario desde el JWT
+    owner: req.user.id
   };
 
-
   database.todos.push(newTodo);
-  res.status(201).json({ todo: newTodo });
+  res.status(201).json({msg:"Tarea creada con éxito", todo: newTodo });
+  console.log("Tarea creada con éxito");
 }
